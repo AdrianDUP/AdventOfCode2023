f89fd25059ffbc2a1e612dce4daafceed14ac3ee
Adrian du Plessis <adrian.du.plessis@basworld.com>
2023-12-13 08:55:33 +0100

    Attempted Day 13 Part 1

---
+++ php/routes/puzzle/13/part1/index.php
@@ -0,0 +1,7 @@
+<?php
+
+use Adrian\AdventOfCode2023\Solver\Thirteen\Part1;
+
+$solver = new Part1();
+
+printSolution($solver->processFile(__DIR__ . '/test1.txt'));
--- php/src/Solver/Thirteen/Part1.php
+++ php/src/Solver/Thirteen/Part1.php
@@ -5,26 +5,40 @@ namespace Adrian\AdventOfCode2023\Solver\Thirteen;
 class Part1
 {
     public function processFile(string $fileName): int {
-        $fileLines = explode(PHP_EOL, trim(file_get_contents($fileName)));
 
-        $columnRows = $htis->makeColumns($fileLines);
+        return $total;
     }
 
     private function checkRows(array $lines, int $currentIndex = 0): ?int {
-        if (empty($lines)) {
-            return null;
+        $lineCount = count($lines);
+
+        if ($lineCount % 2 === 0) {
+
         }
+    }
 
-        $nextLine = array_shift($lines);
+    private function checkForMirrorIndex(array $lines): ?int {
+        $lineCount = count($lines);
 
-        if (in_array($nextLine, $lines)) {
-            return $currentIndex;
-        } else {
-            return $this->checkRows($lines, ++$currentIndex);
+        for ($i = 0; $i < $lineCount; $i++) {
+            if ($lines[0] !== $lines[$lineCount - ($i + 1)]) {
+                return null;
+            }
         }
     }
 
     private function makeColumns(array $lines): array {
-        
+        $rowsFromColumns = [];
+        for($i = 0; $i < strlen($lines[0]); $i++) {
+            if (empty($rowsFromColumns[$i])) {
+                $rowsFromColumns[$i] = '';
+            }
+
+            foreach($lines as $line) {
+                $rowsFromColumns[$i] .= substr($line, $i, 1);
+            }
+        }
+
+        return $rowsFromColumns;
     }
 }
