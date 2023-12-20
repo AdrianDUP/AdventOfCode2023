7d67b22ac6042461721d6e795b4424866aab56f1
Adrian du Plessis <adrian.du.plessis@basworld.com>
2023-12-20 15:56:04 +0100

    further attempt at day 13 php

--- php/routes/puzzle/13/part1/index.php
+++ php/routes/puzzle/13/part1/index.php
@@ -5,3 +5,4 @@ use Adrian\AdventOfCode2023\Solver\Thirteen\Part1;
 $solver = new Part1();
 
 printSolution($solver->processFile(__DIR__ . '/test1.txt'));
+printSolution($solver->processFile(__DIR__ . '/input.txt'));
--- php/src/Solver/Thirteen/Part1.php
+++ php/src/Solver/Thirteen/Part1.php
@@ -5,16 +5,94 @@ namespace Adrian\AdventOfCode2023\Solver\Thirteen;
 class Part1
 {
     public function processFile(string $fileName): int {
+        $total = 0;
+
+        $fileContents = file_get_contents($fileName);
+        $fileLines = explode("\n", $fileContents);
+
+        $currentInput = [];
+
+        foreach($fileLines as $line) {
+            if (empty($line)) {
+                debug('Empty line, starting check', [
+                    'lines' => $currentInput,
+                ]);
+                $result = $this->checkRows($currentInput);
+
+                if (is_null($result)) {
+                    $currentInput = $this->makeColumns($currentInput);
+
+                    $result = $this->checkRows($currentInput);
+
+                    if (is_int($result)) {
+                        debug('Adding to total', [
+                            'result' => $result,
+                        ]);
+                        $total += $result;
+                    }
+                } else {
+                    debug('Adding to total', [
+                        'result' => $result,
+                    ]);
+                    $total += (100 * $result);
+                }
+
+                $currentInput = [];
+                continue;
+            }
+
+            $currentInput[] = $line;
+        }
 
         return $total;
     }
 
-    private function checkRows(array $lines, int $currentIndex = 0): ?int {
-        $lineCount = count($lines);
+    private function checkRows(array $lines): ?int {
+        $firstElement = reset($lines);
+        $matchFound = false;
+        $nextElement = end($lines);
 
-        if ($lineCount % 2 === 0) {
+        while (!$matchFound) {
+            if (0 === key($lines)) {
+                break;
+            }
 
+            if ($nextElement === $firstElement) {
+                $matchFound = true;
+                break;
+            }
+
+            $nextElement = prev($lines);
         }
+
+        if (!$matchFound) {
+            $lastElement = end($lines);
+            $nextElement = reset($lines);
+
+            while (!$matchFound) {
+                if (key($lines) === count($lines) - 1) {
+                    break;
+                }
+                if ($nextElement === $lastElement) {
+                    $matchFound = true;
+                    break;
+                }
+
+                $nextElement = next($lines);
+            }
+
+            if ($matchFound) {
+                $slice = array_slice($lines, key($lines));
+
+                return $this->isPalindrome($slice, 0);
+            }
+        } else {
+            $slice = array_slice($lines, 0, key($lines) + 1);
+
+            return $this->isPalindrome($slice, 0);
+        }
+
+        return null;
     }
 
     private function checkForMirrorIndex(array $lines): ?int {
@@ -51,5 +129,22 @@ class Part1
         }
 
         return $rowsFromColumns;
+    }
+
+    private function isPalindrome(array $lines, int $currentIndex = 0): ?int {
+        debug('Checking for palindrome', [
+            'lines' => $lines,
+            'currentIndex' => $currentIndex,
+        ]);
+        if (count($lines) < 2) {
+            debug('Nothing left to check in palindrome');
+            return $currentIndex + 1;
+        }
+        if ($lines[0] === $lines[count($lines) - 1]) {
+            debug('first and last match');
+            return $this->isPalindrome(array_slice($lines, 1, -1), $currentIndex + 1);
+        }
+
+        return null;
     }
 }
